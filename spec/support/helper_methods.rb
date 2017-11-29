24ee826120b4e518bd6dd82e12c004612f8a4331
Felipe G. Hespanhol <felipe@totalcommit.com>
2017-11-29 20:16:46 -0200

    refactored spec/support/helper_methods.rb

--- spec/support/helper_methods.rb
+++ spec/support/helper_methods.rb
@@ -3,13 +3,17 @@ module HelperMethods
     PaymentGateway::Mundipagg.configure { |config| config.access_key = "1234123412341234" }
   end
 
-  def stub_mundipagg_request(gateway_class, method, request_method, request_url, request_body={})
-    self.config_access_key
+  def request_mock(gateway_class, method)
     request_mock_class = if gateway_class.is_a?(Symbol) || gateway_class.is_a?(String)
                            eval("RequestMocks::Mundipagg::#{gateway_class.capitalize}")
                          else
                            eval("RequestMocks::#{gateway_class.to_s.split('::').last(2)}")
                          end
+    request_mock_class.send(method)
+  end
+
+  def stub_mundipagg_request(gateway_class, method, request_method, request_url, request_body={})
+    self.config_access_key
     stub_request(request_method, request_url).
        with(
          body: request_body.to_json,
@@ -24,7 +28,7 @@ module HelperMethods
        to_return(
          status:  200,
          headers: { 'Content-type' => 'application/json' },
-         body:    request_mock_class.send(method).to_json
+         body:    self.request_mock(gateway_class, method).to_json
        )
   end
 end
